e915260942b5ac43f6189a31acdd84660f6ffb75
Corbin Ko <corbinko@Corbins-MacBook-Pro.local>
2019-12-04 10:23:03 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      
+      <div>
+        <a href="https://youtube.com"><img src="https://img.icons8.com/cotton/2x/folder-invoices.png" /></a>
+        <p>blah</p>
+        <p><a href="#" onclick="window.open('http://google.com');
+    window.open('http://yahoo.com');">Click to open Google and Yahoo</a></p>
+        
+      </div>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
